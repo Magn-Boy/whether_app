c7fe41bc9ee138dddce886f84789676010031029
Magn-Boy <andruha.vasilev.2004@mail.ru>
2025-10-28 16:05:18 +0300

    init: add file for deploy and new package for mini app telegram.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:telegram_web_app/telegram_web_app.dart';
 import 'package:whether_app/app.dart';
 
 import 'core/theme/theme_manager.dart' show ThemeManager;
 
-void main() {
+void main() async {
+  try {
+    if (TelegramWebApp.instance.isSupported) {
+      TelegramWebApp.instance.ready();
+      Future.delayed(
+        const Duration(seconds: 1),
+        TelegramWebApp.instance.expand,
+      );
+    }
+  } catch (e) {
+    print("Error happened in Flutter while loading Telegram $e");
+    await Future.delayed(const Duration(milliseconds: 200));
+    main();
+    return;
+  }
+
+  FlutterError.onError = (details) {
+    print("Flutter error happened: $details");
+  };
+
   final themeManager = ThemeManager();
   runApp(MyApp(themeManager));
 }
